d594da5768bd6704655a6d69d67f7bbce7e96696
Zhi An <zhidkovaayu@gmail.com>
2026-02-28 20:55:47 +0400

    Fixed

--- src/main/java/ru/netology/RestHomework2_my/RestHomework2MyApplication.java
+++ src/main/java/ru/netology/RestHomework2_my/RestHomework2MyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestHomework2MyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestHomework2MyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestHomework2MyApplication.class, args);
+    }
 
 }
